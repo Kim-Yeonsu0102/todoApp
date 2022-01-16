580cd6112c9f05ae8822599d928d43bced63478c
Kim yeon su <rla5258@gmail.com>
2022-01-17 04:54:29 +0900

    style 수정

--- js/todo.js
+++ js/todo.js
@@ -1,16 +1,25 @@
 const todoInput = document.querySelector("#todo-input")
 const todoList = document.querySelector(".todo-list")
+const write = document.querySelector("#write")
 
 
-
-todoInput.addEventListener("keypress",(e)=>{
-    if(e.keyCode === 13){        
+todoInput.addEventListener("keypress", (e) => {
+    if (e.keyCode === 13) {
         generateTodo(todoInput.value);
         todoInput.value = ""
     }
 })
 
-const generateTodo = (todo)=>{
+write.addEventListener("click", () => {
+    generateTodo(todoInput.value);
+    todoInput.value = ""
+})
+
+
+
+
+
+const generateTodo = (todo) => {
     const li = document.createElement("li")
     const likeSpan = generateLike();
     const ItemSpan = generateItem(todo);
@@ -22,7 +31,7 @@ const generateTodo = (todo)=>{
 
 }
 
-const generateLike = ()=>{
+const generateLike = () => {
     const span = document.createElement("span");
     span.classList.add("todo-like")
     const icon = document.createElement("i");
@@ -30,11 +39,10 @@ const generateLike = ()=>{
     icon.classList.add("like")
     icon.innerText = "favorite_border"
     span.appendChild(icon);
-    icon.addEventListener("click",()=>{
-        if(icon.innerText === "favorite_border"){
+    icon.addEventListener("click", () => {
+        if (icon.innerText === "favorite_border") {
             icon.innerText = "favorite"
-        }
-        else{
+        } else {
             icon.innerText = "favorite_border"
         }
     })
@@ -44,16 +52,16 @@ const generateLike = ()=>{
     return span;
 }
 
-const generateItem = (todo)=>{
+const generateItem = (todo) => {
     const span = document.createElement("span");
     span.classList.add("todo-item");
     span.innerText = todo;
 
     return span;
-   
+
 }
 
-const generateMJ = ()=>{
+const generateMJ = () => {
     const span = document.createElement("span");
     span.classList.add("todo-manage")
     const icon1 = document.createElement("i");
@@ -67,38 +75,24 @@ const generateMJ = ()=>{
     icon2.classList.add("clear")
     icon2.innerText = "clear"
 
-icon1.addEventListener("click",(e)=>{
-    const li = e.target.parentNode.parentNode;
-    li.classList.add('done')
-    console.log(li)
+    icon1.addEventListener("click", (e) => {
+        const li = e.target.parentNode.parentNode;
+        li.classList.add('done')
+        console.log(li)
 
-})
-icon2.addEventListener("click",(e)=>{
-    const li = e.target.parentNode.parentNode;
-    const ul = e.target.parentNode.parentNode.parentNode;
-    ul.removeChild(li)
-    console.log("del")
+    })
+    icon2.addEventListener("click", (e) => {
+        const li = e.target.parentNode.parentNode;
+        const ul = e.target.parentNode.parentNode.parentNode;
+        ul.removeChild(li)
+        console.log("del")
 
-})
+    })
 
 
-span.appendChild(icon1)
-span.appendChild(icon2)
+    span.appendChild(icon1)
+    span.appendChild(icon2)
 
     return span;
-  
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
